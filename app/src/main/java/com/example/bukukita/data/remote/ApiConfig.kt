262a2d6ff4344b9660de1b044188ed3becf8c866
Zaacodez <napcodezx@gmail.com>
2026-07-17 10:07:18 +0700

    Delete app/src/main/java/com/example/bukukita/data/remote/ApiConfig.kt

--- app/src/main/java/com/example/bukukita/data/remote/ApiConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bukukita.data.remote
-
-object ApiConfig {
-    const val SUPABASE_URL = "https://rxoqrbidskmwxnzwwdli.supabase.co/"
-    const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4b3FyYmlkc2ttd3huend3ZGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMDY1ODMsImV4cCI6MjA4ODY4MjU4M30.Gm1Ixr7tX4moYdJ5vb_7o21zrYoXutEG763ypFy-dyk"
-    const val GEMINI_API_KEY = "AQ.Ab8RN6IuKmtwOrcJ9w8eqQt9BnBRzpEYtgVmOZ8xnObcLP6G0g"
-}
